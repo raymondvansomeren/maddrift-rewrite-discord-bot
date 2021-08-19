5968023338602be1e7fba2252a73c99aeca4dfe5
Raymond van Someren <raymondvansomeren@live.nl>
2021-08-19 23:18:55 +0200

    Fix sticky

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ async function stickyStuff(message)
                 return console.log(`File read failed: ${err}`);
             }
 
+            if (message.author.bot && message.embeds[0]?.footer === 'Sticky message') return;
             message.channel.messages.fetch({ limit: 20 })
                 .then((msgs) =>
                 {
